733b067d7c54d5acda52829d176660e802de1091
ElBookyes <dtuparov94@gmail.com>
2024-04-26 02:03:55 +0300

    main

---
+++ src/app/app.module.ts
@@ -0,0 +1,16 @@
+// import { NgModule } from '@angular/core';
+// import { BrowserModule } from '@angular/platform-browser';
+// import { FormsModule } from '@angular/forms'; // Import FormsModule
+
+
+// @NgModule({
+//     declarations: [
+//         // Remove AppComponent from declarations array
+//     ],
+//     imports: [
+//         BrowserModule,
+//         FormsModule // Add FormsModule here
+//     ],
+//     providers: [],
+// })
+// export class AppModule { }
